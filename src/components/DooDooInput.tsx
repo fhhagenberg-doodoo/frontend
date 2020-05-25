257deb0b2d3a230a00dfcb30c27ec9896cf0c002
Alexander Stelzhammer <alex.stelzhammer@gmail.com>
2020-05-25 18:54:31 +0200

    Add input field

--- src/components/DooDooInput.tsx
+++ src/components/DooDooInput.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { FaPlus } from "react-icons/fa";
 
 export const DooDooInput: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="flex w-full px-2 py-3 bg-white text-brown rounded-lg">
+      <input className="ml-4 pl-2 flex-1 border-b-2 border-brown" type="text" />
+      <button
+        className="px-4 py-1 h-full align-text-bottom outline-none focus:outline-none focus:border-none"
+        type="submit"
+      >
+        <FaPlus className="outline-none border-none"></FaPlus>
+      </button>
+    </div>
+  );
 };
