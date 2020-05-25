9d188a60afc44f301de93ba0cc60493b9b4b9bfe
Alexander Stelzhammer <alex.stelzhammer@gmail.com>
2020-05-25 20:15:37 +0200

    Restructuring, responsiveness

--- src/components/DooDooTask/DooDooTask.tsx
+++ src/components/DooDooTask/DooDooTask.tsx
@@ -9,7 +9,7 @@ interface DooDooTaskProps {
 
 export const DooDooTask: React.FC<DooDooTaskProps> = ({ task }) => {
   return (
-    <div className="flex px-4 py-2 justify-end bg-white text-brown text-xs lg:text-lg rounded-lg">
+    <div className="flex px-4 py-2 justify-end bg-white text-brown text-md lg:text-lg rounded-lg">
       <DooDooTaskDescription
         title={task.title}
         subTitle={task.subTitle}
